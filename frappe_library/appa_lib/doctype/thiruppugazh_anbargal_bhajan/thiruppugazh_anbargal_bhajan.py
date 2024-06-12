3eb03543dbe7820d721f158758bf8a5877c78ff6
Meenakshisundaram Koushik <koushik.ms@gmail.com>
2024-06-12 23:52:10 +0530

    implement create/update playlist
    introduce video link in song
    introduce playlist link in bhajan
    use youtube data api to create playlist and populate with songs in Bhajan
    setup auth from env or credentials.py

--- frappe_library/appa_lib/doctype/thiruppugazh_anbargal_bhajan/thiruppugazh_anbargal_bhajan.py
+++ frappe_library/appa_lib/doctype/thiruppugazh_anbargal_bhajan/thiruppugazh_anbargal_bhajan.py
@@ -2,33 +2,43 @@
 # For license information, please see license.txt
 
 import os
+import time
+from urllib.parse import urlparse, parse_qs
+
 import frappe
 from frappe.model.document import Document
-import time
+import frappe_library.appa_lib.playlists as playlists
 
 class ThiruppugazhAnbargalBhajan(Document):
 	@frappe.whitelist()
 	def update_playlist(self):
 		print("Playing lists!")
-		token = {}
-		token_str = os.getenv("API_TOKEN_JSON")
-		if token_str:
-			print("Loading credentials from env...")
-			token = eval(token_str)
-		else:
-			print("Credentials not found. Using empty token...")
+		youtube = playlists.get_api_client()
 		num_steps = 1 + len(self.song_list)
 		step = 100 / (num_steps)
 		state = 0
 		frappe.publish_progress(
-			int(state*step),
+			1+int(state*step),
 			title='Updating Playlist',
 			description='Creating empty playlist...',
 			doctype=self.doctype,
 			docname=self.name,
 		)
-		# TODO: delete any existing playlist for this bhajan
-		time.sleep(2)
+		if self.link_to_playlist:
+			print(f"Found existing playlist: {self.link_to_playlist}")
+			query = urlparse(self.link_to_playlist).query
+			playlist_id = parse_qs(query).get("list")
+			if playlist_id:
+				try:
+					playlists.delete(youtube, playlist_id)
+					print("Successfully deleted!")
+				except:
+					print(f"Failed to delete existing playlist: {self.link_to_playlist}")
+				finally:
+					self.link_to_playlist = ""
+
+		response = playlists.create(youtube, title=self.name, description="முருகா ஶரணம். Playlist for practice.")
+		playlist_id = response.get("id")
 		state += 1
 		frappe.publish_progress(
 			int(state*step),
@@ -40,6 +50,8 @@ class ThiruppugazhAnbargalBhajan(Document):
 		time.sleep(1)
 		for entry in self.song_list:
 			print(f"{entry.number}\t{entry.song}")
+			song = frappe.get_doc("Song", entry.song)
+			video_ids = playlists.extract_id(song.video_link_youtube)
 			frappe.publish_progress(
 				int(state*step),
 				title="Updating Playlist",
@@ -47,7 +59,8 @@ class ThiruppugazhAnbargalBhajan(Document):
 				doctype=self.doctype,
 				docname=self.name,
 			)
-			time.sleep(1)
+			if video_ids and len(video_ids) > 0:
+				playlists.insert_video(youtube, playlist_id, video_ids[0])
 			state += 1
 			frappe.publish_progress(
 				int(state*step),
@@ -63,9 +76,11 @@ class ThiruppugazhAnbargalBhajan(Document):
 			doctype=self.doctype,
 			docname=self.name,
 		)
+		self.link_to_playlist = f"https://www.youtube.com/playlist?list={playlist_id}"
+		self.save()
 		frappe.msgprint(
 			title = "Success",
-			msg = "Create playlist on youtube: <a href=\"https://youtu.be\" target=\"blank\">Youtube </a>",
+			msg = f"Created playlist on <a href=\"{self.link_to_playlist}\" target=\"blank\">Youtube </a>",
 			realtime = True,
 			primary_action = {
 				"label": frappe._("Ok"),
