fcfb581abc2e1e1c0b1821d5985fba5020b2b1db
Meenakshisundaram Koushik <koushik.ms@gmail.com>
2024-06-14 20:42:32 +0530

    use spaces instead of tabs

--- frappe_library/appa_lib/doctype/thiruppugazh_anbargal_bhajan/thiruppugazh_anbargal_bhajan.py
+++ frappe_library/appa_lib/doctype/thiruppugazh_anbargal_bhajan/thiruppugazh_anbargal_bhajan.py
@@ -9,82 +9,87 @@ import frappe
 from frappe.model.document import Document
 import frappe_library.appa_lib.playlists as playlists
 
-class ThiruppugazhAnbargalBhajan(Document):
-	@frappe.whitelist()
-	def update_playlist(self):
-		print("Playing lists!")
-		youtube = playlists.get_api_client()
-		num_steps = 1 + len(self.song_list)
-		step = 100 / (num_steps)
-		state = 0
-		frappe.publish_progress(
-			1+int(state*step),
-			title='Updating Playlist',
-			description='Creating empty playlist...',
-			doctype=self.doctype,
-			docname=self.name,
-		)
-		if self.link_to_playlist:
-			print(f"Found existing playlist: {self.link_to_playlist}")
-			query = urlparse(self.link_to_playlist).query
-			playlist_id = parse_qs(query).get("list")
-			if playlist_id:
-				try:
-					playlists.delete(youtube, playlist_id)
-					print("Successfully deleted!")
-				except:
-					print(f"Failed to delete existing playlist: {self.link_to_playlist}")
-				finally:
-					self.link_to_playlist = ""
 
-		response = playlists.create(youtube, title=self.name, description="முருகா ஶரணம். Playlist for practice.")
-		playlist_id = response.get("id")
-		state += 1
-		frappe.publish_progress(
-			int(state*step),
-			title="Updating Playlist",
-			description="Empty Playlist created. Adding songs...",
-			doctype=self.doctype,
-			docname=self.name,
-		)
-		time.sleep(1)
-		for entry in self.song_list:
-			print(f"{entry.number}\t{entry.song}")
-			song = frappe.get_doc("Song", entry.song)
-			video_ids = playlists.extract_id(song.video_link_youtube)
-			frappe.publish_progress(
-				int(state*step),
-				title="Updating Playlist",
-				description=f"Add to playlist: {entry.song}",
-				doctype=self.doctype,
-				docname=self.name,
-			)
-			if video_ids and len(video_ids) > 0:
-				playlists.insert_video(youtube, playlist_id, video_ids[0])
-			state += 1
-			frappe.publish_progress(
-				int(state*step),
-				title="Updating Playlist",
-				description=f"Successfully added {entry.song}!",
-				doctype=self.doctype,
-				docname=self.name,
-			)
-		frappe.publish_progress(
-			100,
-			title="Updating Playlist",
-			description="Updating Bhajan with link to playlist",
-			doctype=self.doctype,
-			docname=self.name,
-		)
-		self.link_to_playlist = f"https://www.youtube.com/playlist?list={playlist_id}"
-		self.save()
-		frappe.msgprint(
-			title = "Success",
-			msg = f"Created playlist on <a href=\"{self.link_to_playlist}\" target=\"blank\">Youtube </a>",
-			realtime = True,
-			primary_action = {
-				"label": frappe._("Ok"),
-				"client_action": "reload_page",
-				"hide_on_success": True,
-			}
-		)
+class ThiruppugazhAnbargalBhajan(Document):
+    @frappe.whitelist()
+    def update_playlist(self):
+        print("Playing lists!")
+        youtube = playlists.get_api_client()
+        num_steps = 1 + len(self.song_list)
+        step = 100 / (num_steps)
+        state = 0
+        frappe.publish_progress(
+            1 + int(state * step),
+            title="Updating Playlist",
+            description="Creating empty playlist...",
+            doctype=self.doctype,
+            docname=self.name,
+        )
+        if self.link_to_playlist:
+            print(f"Found existing playlist: {self.link_to_playlist}")
+            query = urlparse(self.link_to_playlist).query
+            playlist_id = parse_qs(query).get("list")
+            if playlist_id:
+                try:
+                    playlists.delete(youtube, playlist_id)
+                    print("Successfully deleted!")
+                except:
+                    print(
+                        f"Failed to delete existing playlist: {self.link_to_playlist}"
+                    )
+                finally:
+                    self.link_to_playlist = ""
+
+        response = playlists.create(
+            youtube, title=self.name, description="முருகா ஶரணம். Playlist for practice."
+        )
+        playlist_id = response.get("id")
+        state += 1
+        frappe.publish_progress(
+            int(state * step),
+            title="Updating Playlist",
+            description="Empty Playlist created. Adding songs...",
+            doctype=self.doctype,
+            docname=self.name,
+        )
+        time.sleep(1)
+        for entry in self.song_list:
+            print(f"{entry.number}\t{entry.song}")
+            song = frappe.get_doc("Song", entry.song)
+            video_ids = playlists.extract_id(song.video_link_youtube)
+            frappe.publish_progress(
+                int(state * step),
+                title="Updating Playlist",
+                description=f"Add to playlist: {entry.song}",
+                doctype=self.doctype,
+                docname=self.name,
+            )
+            if video_ids and len(video_ids) > 0:
+                playlists.insert_video(youtube, playlist_id, video_ids[0])
+            state += 1
+            frappe.publish_progress(
+                int(state * step),
+                title="Updating Playlist",
+                description=f"Successfully added {entry.song}!",
+                doctype=self.doctype,
+                docname=self.name,
+            )
+        frappe.publish_progress(
+            100,
+            title="Updating Playlist",
+            description="Updating Bhajan with link to playlist",
+            doctype=self.doctype,
+            docname=self.name,
+        )
+        self.link_to_playlist = f"https://www.youtube.com/playlist?list={playlist_id}"
+        self.save()
+        frappe.msgprint(
+            title="Success",
+            msg=f'Created playlist on <a href="{self.link_to_playlist}" target="blank">Youtube </a>',
+            realtime=True,
+            primary_action={
+                "label": frappe._("Ok"),
+                "client_action": "reload_page",
+                "hide_on_success": True,
+            },
+        )
--- frappe_library/appa_lib/playlists.py
+++ frappe_library/appa_lib/playlists.py
@@ -5,128 +5,131 @@ import google.oauth2.credentials
 import googleapiclient.discovery
 
 scopes = [
-	"https://www.googleapis.com/auth/userinfo.profile",
-	"https://www.googleapis.com/auth/youtube",
-	"openid",
-	"https://www.googleapis.com/auth/userinfo.email",
-	"https://www.googleapis.com/auth/youtube.force-ssl",
-	"https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/youtube",
+    "openid",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+    "https://www.googleapis.com/auth/youtube.readonly",
 ]
 
 videos = [
-	"https://www.youtube.com/watch?v=QwZ4HGgcFz8&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-",
-	"https://www.youtube.com/watch?v=psd4rJvBF8E&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=2",
-	"https://www.youtube.com/watch?v=qYgmDdSzTNs&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=3",
-	"https://www.youtube.com/watch?v=ba3125ppXXU&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=4",
-	"https://www.youtube.com/watch?v=CahOdjudiTI&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=5",
-	"https://www.youtube.com/watch?v=eSYhqiMt_lw&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=6",
-	"https://www.youtube.com/watch?v=kV-q5R4DP24&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=7",
-	"https://www.youtube.com/watch?v=wCO783BxZjA&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=8",
-	"https://www.youtube.com/watch?v=MWrLzO5LdVg&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=9",
+    "https://www.youtube.com/watch?v=QwZ4HGgcFz8&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-",
+    "https://www.youtube.com/watch?v=psd4rJvBF8E&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=2",
+    "https://www.youtube.com/watch?v=qYgmDdSzTNs&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=3",
+    "https://www.youtube.com/watch?v=ba3125ppXXU&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=4",
+    "https://www.youtube.com/watch?v=CahOdjudiTI&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=5",
+    "https://www.youtube.com/watch?v=eSYhqiMt_lw&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=6",
+    "https://www.youtube.com/watch?v=kV-q5R4DP24&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=7",
+    "https://www.youtube.com/watch?v=wCO783BxZjA&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=8",
+    "https://www.youtube.com/watch?v=MWrLzO5LdVg&list=PL1hdtOv5_bcceSo65HWoU51wmLVBNK2C-&index=9",
 ]
 
 
 def list(youtube):
-	request = youtube.playlists().list(
-		part="snippet", channelId="UCo5V3CHVRTl_c_BkJtpoDaQ"
-	)
-	return request.execute()
+    request = youtube.playlists().list(
+        part="snippet", channelId="UCo5V3CHVRTl_c_BkJtpoDaQ"
+    )
+    return request.execute()
 
 
 def extract_id(url) -> str:
-	query = urlparse(url).query
-	return parse_qs(query).get("v")
+    query = urlparse(url).query
+    return parse_qs(query).get("v")
+
 
 def get_video_ids():
-	return [id for v in map(extract_id, videos) for id in v if v]
+    return [id for v in map(extract_id, videos) for id in v if v]
 
 
-def create(youtube, title="Muruga Saranam", description="Vetrivael Muruganukku! Arohara!"):
-	request = youtube.playlists().insert(
-		part="snippet,status,id",
-		body={
-			"snippet": {
-				"title": title,
-				"description": description,
-			},
-			"status": {"privacyStatus": "public"},
-		},
-	)
-	return request.execute()
+def create(
+    youtube, title="Muruga Saranam", description="Vetrivael Muruganukku! Arohara!"
+):
+    request = youtube.playlists().insert(
+        part="snippet,status,id",
+        body={
+            "snippet": {
+                "title": title,
+                "description": description,
+            },
+            "status": {"privacyStatus": "public"},
+        },
+    )
+    return request.execute()
 
 
 def insert_video(youtube, playlist_id, video_id):
-	request = youtube.playlistItems().insert(
-		part="snippet",
-		body={
-			"snippet": {
-				"playlistId": playlist_id,
-				"resourceId": {"kind": "youtube#video", "videoId": video_id},
-			}
-		},
-	)
-	return request.execute()
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": playlist_id,
+                "resourceId": {"kind": "youtube#video", "videoId": video_id},
+            }
+        },
+    )
+    return request.execute()
 
 
 def insert_videos(youtube, playlist_id):
-	res = []
-	for id in get_video_ids():
-		res.append(insert_video(youtube, playlist_id, id))
-	return res
+    res = []
+    for id in get_video_ids():
+        res.append(insert_video(youtube, playlist_id, id))
+    return res
 
 
 def delete(youtube, playlist_id):
-	request = youtube.playlists().delete(id=playlist_id)
-	return request.execute()
+    request = youtube.playlists().delete(id=playlist_id)
+    return request.execute()
 
 
 def get_api_client():
-	api_service_name = "youtube"
-	api_version = "v3"
-	token = {}
-	token_str = os.getenv("API_TOKEN_JSON")
-	if token_str:
-		token = eval(token_str)
-	else:
-		import credentials
+    api_service_name = "youtube"
+    api_version = "v3"
+    token = {}
+    token_str = os.getenv("API_TOKEN_JSON")
+    if token_str:
+        token = eval(token_str)
+    else:
+        import credentials
 
-		token = credentials.token
-	# Get credentials and create an API client
-	credentials = google.oauth2.credentials.Credentials(**token)
-	youtube = googleapiclient.discovery.build(
-		api_service_name, api_version, credentials=credentials
-	)
-	return youtube
+        token = credentials.token
+    # Get credentials and create an API client
+    credentials = google.oauth2.credentials.Credentials(**token)
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials
+    )
+    return youtube
 
 
 def main():
-	youtube = get_api_client()
-	response = ""
+    youtube = get_api_client()
+    response = ""
 
-	command = sys.argv[1]
-	if command == "list":
-		response = list(youtube)
-	elif command == "create":
-		response = create(youtube)
-	elif command == "delete":
-		playlist_id = sys.argv[2]
-		print(f"Deleting playlist ID: [{playlist_id}]...")
-		response = delete(youtube, playlist_id)
-	elif command == "fill":
-		playlist_id = sys.argv[2]
-		print(f"Adding videos to playlist ID: [{playlist_id}]")
-		response = insert_videos(youtube, playlist_id)
-	elif command == "publish":
-		print("Creating Playlist...")
-		response = create(youtube)
-		playlist_id = response.get("id")
-		if playlist_id:
-			print(f"Created playlist: [{playlist_id}]. Adding videos to it...")
-			response = insert_videos(youtube, playlist_id)
-	else:
-		print(f"Unknown command: {command}")
-	print(json.dumps(response))
+    command = sys.argv[1]
+    if command == "list":
+        response = list(youtube)
+    elif command == "create":
+        response = create(youtube)
+    elif command == "delete":
+        playlist_id = sys.argv[2]
+        print(f"Deleting playlist ID: [{playlist_id}]...")
+        response = delete(youtube, playlist_id)
+    elif command == "fill":
+        playlist_id = sys.argv[2]
+        print(f"Adding videos to playlist ID: [{playlist_id}]")
+        response = insert_videos(youtube, playlist_id)
+    elif command == "publish":
+        print("Creating Playlist...")
+        response = create(youtube)
+        playlist_id = response.get("id")
+        if playlist_id:
+            print(f"Created playlist: [{playlist_id}]. Adding videos to it...")
+            response = insert_videos(youtube, playlist_id)
+    else:
+        print(f"Unknown command: {command}")
+    print(json.dumps(response))
 
 
 if __name__ == "__main__":
-	main()
+    main()
